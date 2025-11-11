3de21d58f123fb7de38be24f19ad6e83caed2a30
Curtis Hinds <info@ironmaidenrealty.com>
2025-11-11 02:29:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1147747666-cd974",
+  "appId": "1:520079579947:web:4a25fb019cba3f7b98c36e",
+  "apiKey": "AIzaSyBtT-1Z6hsri6JNOgVjDgmHTGRB9DIeRDQ",
+  "authDomain": "studio-1147747666-cd974.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "520079579947"
+};
